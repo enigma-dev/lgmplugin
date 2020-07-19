3c49771892945f700bcc9a2261ce5693d03453d0
Robert Colton <robertbcolton@gmail.com>
2020-07-18 21:50:02 -0400

    Update EFileReader.java

--- org/enigma/file/EFileReader.java
+++ org/enigma/file/EFileReader.java
@@ -49,7 +49,6 @@ import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JProgressBar;
 import javax.swing.filechooser.FileView;
 
 import org.enigma.EnigmaRunner;
@@ -551,11 +550,11 @@ public class EFileReader
 	public static void readEgmFile(EProjectFile f, ProjectFile gf, ResNode root) throws IOException
 		{
 		gf.format = EFileWriter.FLAVOR_EGM;
-		JProgressBar progressBar = LGM.getProgressDialogBar();
-		progressBar.setMaximum(f.getEntries().size());
-		LGM.setProgressTitle(Messages.getString("ProgressDialog.EGM_LOADING"));
+		//JProgressBar progressBar = LGM.getProgressDialogBar();
+		//progressBar.setMaximum(f.getEntries().size());
+		//LGM.setProgressTitle(Messages.getString("ProgressDialog.EGM_LOADING"));
 
-		LGM.setProgress(0,Messages.getString("ProgressDialog.ENTRIES"));
+		//LGM.setProgress(0,Messages.getString("ProgressDialog.ENTRIES"));
 		readNodeChildren(f,gf,root,null,new String());
 
 		readResource(f,gf,null,EnigmaSettings.class,
@@ -564,7 +563,7 @@ public class EFileReader
 		while (!postpone.isEmpty())
 			postpone.remove().invoke();
 
-		LGM.setProgress(progressBar.getMaximum(),Messages.getString("ProgressDialog.FINISHED"));
+		//LGM.setProgress(progressBar.getMaximum(),Messages.getString("ProgressDialog.FINISHED"));
 		}
 
 	// Workhorse methods
@@ -647,7 +646,7 @@ public class EFileReader
 				else
 					System.out.println("Extraneous TOC entry: " + e.name + " (" + e.kind + ")");
 				}
-			LGM.setProgress(LGM.getProgressDialogBar().getValue() + 1,Messages.getString("ProgressDialog.ENTRIES"));
+			//LGM.setProgress(LGM.getProgressDialogBar().getValue() + 1,Messages.getString("ProgressDialog.ENTRIES"));
 			}
 		}
 
